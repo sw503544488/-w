0e4985b11350abcf346dfc628420b6a9cc1c8032
sw503544488 <503544488@qq.com>
2021-08-01 16:37:16 +0800

    save

--- src/x.js
+++ src/x.js
@@ -1,4 +1,4 @@
 export default 'xxx'
-
+import "./style.css"
 var a = 'asd'
 console.log(a)
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,21 @@
+var HtmlWebpackPlugin = require('html-webpack-plugin')
 var path = require('path')
 module.exports = {
   mode: 'development', //开发中使用development 会加很多注释, 发布的就改成production
   entry: './src/index.js', //输入
   output: { //输出
     filename: '[name].[contenthash].js',
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, 'dist')
+  },
+  plugins: [new HtmlWebpackPlugin({
+    title: 'frank',
+    filename: 'index1.html',
+    template: 'src/assets/index.html' //用test.html这个模版生成html
+  })],
+  module: {
+    rules: [{
+      test: /\.css$/i,
+      use: ["style-loader", "css-loader"],
+    }, ],
   },
 }
