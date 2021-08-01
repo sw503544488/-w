ae037bc79ed68985a278cdd286f4a8c324d3cd5e
sw503544488 <503544488@qq.com>
2021-08-01 17:11:58 +0800

    add

--- webpack.config.js
+++ webpack.config.js
@@ -7,6 +7,10 @@ module.exports = {
     filename: '[name].[contenthash].js',
     path: path.resolve(__dirname, 'dist')
   },
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: './dist',
+  },
   plugins: [new HtmlWebpackPlugin({
     title: 'frank',
     filename: 'index1.html',
